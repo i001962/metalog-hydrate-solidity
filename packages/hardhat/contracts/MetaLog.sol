02a0bee8897965069ac70ad0942ff455286915d8
i001962@gmail.com <i001962@gmail.com>
2022-08-17 16:06:17 -0700

    cleaner hrd random

--- packages/hardhat/contracts/MetaLog.sol
+++ packages/hardhat/contracts/MetaLog.sol
@@ -163,80 +163,21 @@ contract MetaLog {
         console.logInt(remainder);
         return remainder;
     }
-    
-/*     function getHdrRandom(int256 entityId, int256 varId, int256 option1, int256 option2, int256 pmIndex) external view returns (int256 randi) { 
-    // randi =( mod(( mod( mod( 999999999999989, mod( pmIndex*2499997 + ( varId )*1800451 + ( entityId )*2000371 + ( option1 ) *1796777 + ( option2 )*2299603, 7450589 ) * 4658 + 7450581 ) * 383, 99991 )* 7440893 + mod( mod( 999999999999989, mod( PM_Index*2246527 + ( varId )*2399993 + ( entityId )*2100869 + ( option1 )*1918303 + ( option2 )*1624729, 7450987 ) * 7580 + 7560584 ) * 17669, 7440893 )) * 1343, 4294967296 ) + 0.5 ) / 4294967296;
-        int256 firstMod = mod((pmIndex * 0x0211651be2dc371c540000) + ( varId * 0x017d42a4efb5c53a2c0000) + ( entityId * 0x01a79854466036d7ec0000) + ( option1  * 0x017c7b79f1a774e9040000) + ( option2 * 0x01e6f5ba79e9218f6c0000), 0x0629b93d2f6ba8dd540000 );
-        int256 secondModDivisorA = firstMod.mul(0xfc82bc50dbb9880000); //??
-        int256 secondModDivisorAadd = secondModDivisorA + 0x0629b8ce29b60ba2340000;
-        console.log("secondModDivisor: " );
-        console.logInt(secondModDivisorA);
-        console.logInt(secondModDivisorAadd);
-        //console.logInt(secondModDivisorB);
-
-        int256 secondMod =  0x314dc6448d92a0198151ceb40000 % secondModDivisorAadd; // mod  
-        console.log("remainder mod 2: " );
-        console.logInt(secondMod);
-        int256 secondMod383 = secondModDivisorAadd.mul(0x14c33156c76e9c0000);
-        console.log("secondMod383: " );
-        console.logInt(secondMod383);
-
-        int256 deepMod = mod((pmIndex*0x01dbb87902b1d4c69c0000) + ( varId *0x01fc37e198201d88440000) + ( entityId *0x01bce05631aafd6ef40000) + ( option1 *0x01963769fc2f1d121c0000) + ( option2 *0x01580cbc4ea4d3f2c40000), 0x0629ced08b76f71acc0000 );
-        console.logInt(deepMod);
-
-        randi = (((( secondMod383 % 0x152c85e174fa13fc0000) 
-                    * 0x0627ab9e279744f1d40000 + 
-                    ( ((( 0x314dc6448d92a0198151ceb40000 % deepMod)
-                            * 0x019ae9a1af7054f00000) + 0x0641041474852c41200000 
-                        * 0x03bdd6a1fda038f40000) % 0x0627ab9e279744f1d40000 )
-                    ) * 0x48cdde787b259c0000 % LARGE_PRIME ) + 0x06f05b59d3b20000 ).div(LARGE_PRIME);
-                
-        console.log("randi: " );
-        console.logInt(randi);
-    return randi;
-    } */
 
     function getHdrRandom(int256 entityId, int256 varId, int256 option1, int256 option2, int256 pmIndex) external view returns (int256 randi) { 
         // supports 4 variables for sipmath 3.0 standard
         int256 temp111 = (((pmIndex * 0x0211651be2dc371c540000) + (varId * 0x017d42a4efb5c53a2c0000) + (entityId * 0x01a79854466036d7ec0000) + (option1 * 0x017c7b79f1a774e9040000) + (option2 * 0x01e6f5ba79e9218f6c0000)) / (10 ** 18) ) % 0x0629b93d2f6ba8dd540000;
-        //int256 tempMod = (temp111 / (10 ** 18) )% 0x0629b93d2f6ba8dd540000 ;
-        console.log("temp111: ");
-        console.logInt(temp111);
-        int256 temp11 = add(TEMPCONST,(temp111.mul(0xfc82bc50dbb9880000)));// (0x0629b8ce29b60ba2340000); //% 0x314dc6448d92a0198151ceb40000  ; 
-        console.log("temp11: ");
-        console.logInt(temp11); 
-        int256 temp1Holder = 0x314dc6448d92a0198151ceb40000 % temp11;// 0x14c33156c76e9c0000) % 0x152c85e174fa13fc0000;
-        //console.log("temp1: ");
-        //console.logInt(temp1Holder);
+        int256 temp11 = add(TEMPCONST,(temp111.mul(0xfc82bc50dbb9880000)));
+        int256 temp1Holder = 0x314dc6448d92a0198151ceb40000 % temp11;
         int256 temp1 = temp1Holder.mul(0x14c33156c76e9c0000) % 0x152c85e174fa13fc0000;
-        console.log('temp1');
-        console.logInt(temp1);
-        //int256 holsii = temp1.mul(0x0627ab9e279744f1d40000);
-        //console.logInt(holsii);
         int256 temp222 = (((pmIndex * 0x01dbb87902b1d4c69c0000) + (varId * 0x01fc37e198201d88440000) + (entityId * 0x01bce05631aafd6ef40000) + (option1 * 0x01963769fc2f1d121c0000) + (option2 * 0x01580cbc4ea4d3f2c40000)) / (10 ** 18)) % 0x0629ced08b76f71acc0000;
-        console.log("temp222: ");
-        console.logInt(temp222);
-        int256 temp22 =(temp222.mul(0x019ae9a1af7054f00000)) + 0x0641041474852c41200000; // 0x314dc6448d92a0198151ceb40000 %  0x0629ced08b76f71acc0000
-        console.log("temp22: ");
-        console.logInt(temp22);
+        int256 temp22 =(temp222.mul(0x019ae9a1af7054f00000)) + 0x0641041474852c41200000;
         int256 temp2Holder = 0x314dc6448d92a0198151ceb40000 % temp22;
         int256 temp2 = temp2Holder.mul( 0x03bdd6a1fda038f40000) % 0x0627ab9e279744f1d40000;
-        console.log("temp2: ");
-        console.logInt(temp2);
         int256 temp = temp1.mul(0x0627ab9e279744f1d40000) + temp2;
         int256 temper = temp * 0x48cdde787b259c0000;
-        //) / (10 ** 18) % LARGE_PRIME;
-        console.log("temper: ");
-        console.logInt(temper); // ok to here, check decimals
-        //console.logInt(0x06f05b59d3b20000);  //0.5 ??
-
-        int256 finalHolder =  mod(temper,LARGE_PRIME); //0x06f05b59d3b20000;
-        console.log("finalHolder: ");
-        console.logInt(finalHolder);
-        //randi = finalHolder.div(LARGE_PRIME);
+        int256 finalHolder =  mod(temper,LARGE_PRIME);
         randi = finalHolder.div(LARGE_PRIME);
-        console.log("randi: ");
-        console.logInt(randi);
 
         return randi;
     }  
